7b00a541390ff5afd92ce393aa6a08e1988e71cd
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-09-21 16:13:44 -0600

    uP

--- app.py
+++ app.py
@@ -4,24 +4,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+     return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    if 'video' not in request.files:
-        return 'No video part', 400
-    file = request.files['video']
-    if file.filename == '':
-        return 'No selected video', 400
-    if file:
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        process_video(filename)
-        return 'Video uploaded and processed successfully!'
+     if 'video' not in request.files:
+         return 'No video part', 400
+     file = request.files['video']
+     if file.filename == '':
+         return 'No selected video', 400
+     if file:
+         filename = secure_filename(file.filename)
+         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+         # Aquí puedes llamar a un script o función que use docker-compose para procesar el video
+         process_video(filename)
+         return 'Video uploaded and processed successfully!'
 
 @app.route('/download/<filename>')
 def download_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+     app.run(debug=True)
